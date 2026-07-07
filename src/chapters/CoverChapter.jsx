323264b279d0cf916a444910420ebf5975348973
Rafly <fthelittleprince@gmail.com>
2026-07-07 19:57:31 +0700

    Update button text from 'Taman Mama' to 'Taman'

--- src/chapters/CoverChapter.jsx
+++ src/chapters/CoverChapter.jsx
@@ -210,7 +210,7 @@ export default function CoverChapter({ onFirstInteraction, onContinue }) {
                 whileTap={{ scale: 0.97 }}
                 transition={{ type: 'spring', stiffness: 400, damping: 22 }}
               >
-                Masuk ke Taman Mama
+                Masuk ke Taman
                 <ArrowRight size={16} />
               </motion.button>
             </motion.div>
